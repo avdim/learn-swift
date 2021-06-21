244c48f316c10f0abae3b33b31ff3c552f406123
avdim <avdim@mail.ru>
2021-06-21 14:34:27 +0300

    init

---
+++ learnswift/learnswift/learnswiftApp.swift
@@ -0,0 +1,18 @@
+//
+//  learnswiftApp.swift
+//  learnswift
+//
+//  Created by Dim on 21.06.2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct learnswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
