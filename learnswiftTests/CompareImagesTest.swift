87ba21e87bfb9b6d126c16422989712f87a49eb7
avdim <avdim@mail.ru>
2021-06-22 17:44:14 +0300

    compareScreenshots stub

--- learnswiftTests/CompareImagesTest.swift
+++ learnswiftTests/CompareImagesTest.swift
@@ -8,14 +8,19 @@ import XCTest
 class CompareImagesTest: XCTestCase {
 
     func testCompareSuccessImages() {
-        let imageGH = getProjectDirImage(imagePath: "compare/github/MainScreenUITests/testEmptyArrivalStationAlert.1.png")
+        let imageGH: CGImage = getProjectDirImage(imagePath: "compare/github/MainScreenUITests/testEmptyArrivalStationAlert.1.png")
         let imageLocal = getProjectDirImage(imagePath: "compare/local/MainScreenUITests/testEmptyArrivalStationAlert.1.png")
-        print("imageGH.width: ", imageGH.width)
-        print("imageLocal.width: ", imageLocal.width)
+        XCTAssertTrue(compareScreenshots(expect: imageLocal, actual: imageGH))
     }
 
     func testCompareFailedImages() {
-
+        let imageGH = getProjectDirImage(imagePath: "compare/github/InfoUITests/testSchedulePastDateActionSheet.1.png")
+        let imageLocal = getProjectDirImage(imagePath: "compare/local/InfoUITests/testSchedulePastDateActionSheet.1.png")
+        XCTAssertFalse(compareScreenshots(expect: imageLocal, actual: imageGH))
     }
 
+}
+
+func compareScreenshots(expect: CGImage, actual: CGImage) -> Bool { //todo return diff image
+    return true
 }
